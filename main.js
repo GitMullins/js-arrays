18397a24ff9decbe512bb02212c950a56a5ab260
David Mullins <Mullins6323@gmail.com>
2019-03-05 19:30:27 -0600

    added buildDinos()

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-console.log(hi);
+const dinos = [
+    {
+        dinoType: 't-rex'
+    },
+    {
+        dinoType: 'alligator'
+    },
+    {
+        dinoType: 'wannabe t-rex'
+    }
+];
+
+//loop over dinos
+//build up a domstring
+//print domstring to the dom
+
+const printToDom = (divId, print)=>{
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = print;
+}
+
+const buildDinos = () => {
+    let domString ='';
+    for (let i=0; i < dinos.length; i++){
+        domString += `<h3>${dinos[i].dinoType}</h3>`
+        console.log(dinos[i].dinoType);   
+    }
+    printToDom('dino-barn', domString);
+}
+
+const init = () => {
+    buildDinos();
+}
+
+init();
